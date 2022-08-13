35e81dba980d30c21e00ef93005e1aeeed283414
Basilio <basilio.david.g@gmail.com>
2022-08-13 12:36:02 +0200

    feat(bffe): first state controller

--- bffe/src/app.controller.ts
+++ bffe/src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('calendars')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('v1/post')
+  @UseInterceptors(FileInterceptor('photos'))
+  addPhoto(
+    @Req() expressRequest: Request,
+    @UploadedFile() data: Express.Multer.File,
+  ): void {
+    console.log('file:', data);
   }
 }
