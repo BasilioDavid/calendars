11262319f68a0923b7d45bffdc5398fa9f6937b4
Basilio <basilio.david.g@gmail.com>
2022-10-12 09:00:49 +0200

    feat(bffe/database): add calendars table

---
+++ bffe/src/shared/database/entities/calendar.entity.ts
@@ -0,0 +1,7 @@
+export interface Calendar {
+  id: number;
+  extId: string;
+  name: string;
+  userId: number;
+  statusId: number;
+}
--- bffe/src/shared/database/entities/database.entity.ts
+++ bffe/src/shared/database/entities/database.entity.ts
@@ -1,7 +1,9 @@
+import { Calendar } from './calendar.entity';
 import { ImageEntity } from './image.entity';
 import { UserEntity } from './user.entity';
 
 export class Database {
   image: ImageEntity;
   user: UserEntity;
+  calendar: Calendar;
 }
