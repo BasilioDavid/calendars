e9a2370c00c166603ca575033c9a91a3f3dc3125
Basilio <basilio.david.g@gmail.com>
2022-11-12 10:38:27 +0100

    feat(bffe/database): add user-status table

--- bffe/src/shared/database/entities/database.entity.ts
+++ bffe/src/shared/database/entities/database.entity.ts
@@ -1,9 +1,11 @@
 import { Calendar } from './calendar.entity';
 import { ImageEntity } from './image.entity';
+import { UserStatusEntity } from './user-status.entity';
 import { UserEntity } from './user.entity';
 
 export class Database {
   image: ImageEntity;
   user: UserEntity;
   calendar: Calendar;
+  userStatus: UserStatusEntity;
 }
