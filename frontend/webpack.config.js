67912993f0b32c5f9614cd5cb9128a5840e2ce5d
Basilio <basilio.david.g@gmail.com>
2022-11-12 10:40:52 +0100

    build(frontend/webpack): minify css html and css

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,32 +1,9 @@
 const path = require('path');
-const webpack = require('webpack'); //to access built-in plugins
 const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-// module.exports = {
-//   entry: {
-//     register: './src/register/app.js',
-//     login: './src/login/app.js',
-//     deskCalendars: './src/desk-calendar/app.js',
-//     hub: './src/hub/app.js',
-//     'create-new-calendar': './src/create-new-calendar/app.js',
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: '[name].webpack.bundle.js',
-//   },
-//   devtool: false,
-//   plugins: [
-//     new webpack.SourceMapDevToolPlugin({
-//       filename: 'sourcemaps/[file].map',
-//       publicPath: 'https://example.com/project/',
-//       fileContext: 'public',
-//     }),
-//   ],
-// };
 
 module.exports = {
   entry: {
@@ -36,10 +13,7 @@ module.exports = {
     hub: './src/hub/app.js',
     'create-new-calendar': './src/create-new-calendar/app.js',
   },
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: '[name].webpack.bundle.js',
-  // },
+  output: {},
   devtool: 'source-map',
   module: {
     rules: [
@@ -47,10 +21,6 @@ module.exports = {
         test: /\.html$/i,
         type: 'asset/resource',
       },
-      // {
-      //   test: /\.js$/i,
-      //   type: 'asset/resource',
-      // },
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
@@ -69,29 +39,6 @@ module.exports = {
           from: './src/**/*.css',
         },
       ],
-    }),
-    // new webpack.SourceMapDevToolPlugin({
-    //   filename: 'sourcemaps/[file].map',
-    //   publicPath: 'https://example.com/project/',
-    //   fileContext: 'public',
-    // }),
-    new MiniCssExtractPlugin({
-      // filename: './src/**/*.css',
-    }),
-
-    new UglifyJsPlugin({
-      minify(file, sourceMap) {
-        const extractedComments = [];
-
-        // Custom logic for extract comments
-
-        const { error, map, code, warnings } = require('uglify-js') // Or require('./path/to/uglify-module')
-          .minify(file, {
-            /* Your options for minification */
-          });
-
-        return { error, map, code, warnings, extractedComments };
-      },
     }),
   ],
   optimization: {
@@ -117,18 +64,19 @@ module.exports = {
       }),
 
       new UglifyJsPlugin({
-        // sourceMap: true,
-        // uglifyOptions: {
-        //   warnings: false,
-        //   parse: {},
-        //   compress: {},
-        //   mangle: true, // Note `mangle.properties` is `false` by default.
-        //   output: null,
-        //   toplevel: false,
-        //   nameCache: null,
-        //   ie8: false,
-        //   keep_fnames: true,
-        // },
+        sourceMap: true,
+        cache: true,
+        uglifyOptions: {
+          //   warnings: false,
+          //   parse: {},
+          //   compress: {},
+          //   mangle: true, // Note `mangle.properties` is `false` by default.
+          //   output: null,
+          //   toplevel: false,
+          //   nameCache: null,
+          //   ie8: false,
+          keep_fnames: false,
+        },
       }),
     ],
   },
