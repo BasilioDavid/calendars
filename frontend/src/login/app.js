9e33ecfa4221c652d7c6648fa1eebe9ab35adacc
Basilio <basilio.david.g@gmail.com>
2022-12-01 18:12:36 +0100

    feat(frontend/login): add toast integration and error handling

--- frontend/src/login/app.js
+++ frontend/src/login/app.js
@@ -1,6 +1,7 @@
 import { ENVIRONMENT } from '../common/const';
 import { token as tokenService } from '../common/token.service';
 import { sendForm, preventDefault } from '../common/utils';
+import { generateErrorToast, generateSuccessToast } from '../common/toast';
 
 const API_URL = `${ENVIRONMENT.API_URL}/user/login`;
 
@@ -8,26 +9,40 @@ tokenService.clear();
 
 const emailField = document.getElementById('email');
 const passwordField = document.getElementById('password');
+const button = document.getElementById('send');
+
+function checkButtonEnabled() {
+  button.disabled = !(
+    emailField.value.length !== 0 && passwordField.value.length !== 0
+  );
+}
+
+emailField.addEventListener('input', checkButtonEnabled);
+passwordField.addEventListener('input', checkButtonEnabled);
 
 async function login(event) {
   preventDefault(event);
-  let token;
-  try {
-    token = await sendForm({
-      url: API_URL,
-      method: 'POST',
-      body: {
-        email: emailField.value,
-        password: passwordField.value,
-      },
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (e) {
-    console.error(e);
-    throw e;
+  const token = await sendForm({
+    url: API_URL,
+    method: 'POST',
+    body: {
+      email: emailField.value,
+      password: passwordField.value,
+    },
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (typeof token.errorCode !== 'undefined') {
+    errorHandling(token);
+  } else {
+    tokenService.set(token);
+    window.open('/hub?login=true', '_self');
   }
-  tokenService.set(token);
-  window.open('/hub', '_self');
+}
+
+function errorHandling(error) {
+  console.log('mostrando error de no usuario');
+  if (error.errorCode === 'USERNOTFOUND')
+    generateErrorToast('Combinación de Usuario y contraseña erronea');
 }
 
 window.login = login;
