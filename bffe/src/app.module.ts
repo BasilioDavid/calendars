2f213fe17ae27a21d2e74fb93d36614131bd7650
Basilio <basilio.david.g@gmail.com>
2022-12-03 07:48:29 +0100

    fix(bffe): small fixes

--- bffe/src/app.module.ts
+++ bffe/src/app.module.ts
@@ -3,13 +3,14 @@ import { CalendarModule } from './contexts/calendars/calendars.module';
 import { ImagesModule } from './contexts/images/images.module';
 import { LivecheckModule } from './contexts/livecheck/livecheck.module';
 import { LoginModule } from './contexts/login/login.module';
-import { RegisterModule } from './contexts/users/register/register.module';
+import { RegisterModule } from './contexts/register/register.module';
 import { AuthModule } from './shared/auth/auth.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { ContextModule } from './shared/context/context.module';
 import { GenerateCalendarModule } from './contexts/generate-calendar/images.module';
 import { LoggingModule } from './shared/logging/logging.module';
 import { ErrorModule } from './shared/errors/error.module';
+import { OrderModule } from './contexts/order/order.module';
 
 @Module({
   imports: [
