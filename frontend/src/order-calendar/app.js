77f46b33912c6aa115ccaaf5200cc7994c44a0f3
Basilio <basilio.david.g@gmail.com>
2022-12-03 07:50:31 +0100

    fix(frontend/order-calendar): small fixes and add error handling

--- frontend/src/order-calendar/app.js
+++ frontend/src/order-calendar/app.js
@@ -1,10 +1,10 @@
-import { ENVIRONMENT } from '../common/const';
+import { ENVIRONMENT, ROUTES } from '../common/const';
 import { mobileGuard } from '../common/mobile.guard';
 import { unregisteredUserGuard } from '../common/unregistered-user.guard';
 import { token } from '../common/token.service';
 import { sendForm, preventDefault } from '../common/utils';
 
-const API_URL = `${ENVIRONMENT.API_URL}/calendar/order`;
+const API_URL = `${ENVIRONMENT.API_URL}/order`;
 mobileGuard();
 unregisteredUserGuard();
 
@@ -12,8 +12,8 @@ const userToken = token.get();
 const parameters = new URLSearchParams(window.location.search);
 const calendarId = parameters.get('calendarId');
 
-if (typeof calendarId === 'undefined') {
-  window.location.replace('/hub');
+if (calendarId === null) {
+  window.location.replace(ROUTES.hub);
 }
 
 const cp = document.getElementById('cp');
@@ -30,17 +30,27 @@ async function send(event) {
     url: API_URL,
     method: 'POST',
     body: {
-      cp: cp.value,
+      postalCode: cp.value,
       city: city.value,
       contactNumber: contactNumber.value,
       direction: direction.value,
       specifications: specifications.value,
       instructions: instructions.value,
       paymentMethod: paymentMethod.value,
+      calendarId,
     },
     headers: { 'Content-Type': 'application/json', authorization: userToken },
   });
-  window.open('/hub', '_self');
+  if (typeof token.errorCode !== 'undefined') {
+    errorHandling(token);
+  } else {
+    window.open(`${ROUTES.hub}/?order=true`, '_self');
+  }
 }
 
+function errorHandling(error) {
+  if (error.errorCode === 'USERNOTFOUND')
+    return generateErrorToast('Combinación de Usuario y contraseña erronea');
+  generateErrorToast('Un error desconocido ha sucedido');
+}
 window.send = send;
