73a92fa936da326eaa6bc7ce45fc599fd51ea93f
Basilio <basilio.david.g@gmail.com>
2022-12-04 22:02:58 +0100

    feat(frontend/common): add calendar to name const

--- frontend/src/common/const.js
+++ frontend/src/common/const.js
@@ -11,3 +11,10 @@ export const ROUTES = {
   register: '/register',
   orderCalendar: '/order-calendar',
 };
+
+export const CALENDAR_STATUS_ID_TO_NAME = {
+  1: 'Editando',
+  2: 'Enviado',
+  4: 'Pedido',
+  5: 'Entregado',
+};
