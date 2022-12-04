044b8e23e17ee548e6e5f0d82a2c4c1219759a35
Basilio <basilio.david.g@gmail.com>
2022-12-04 22:05:10 +0100

    fix(frontend/order-calendar): improve user experience and error hanling

--- frontend/src/order-calendar/app.js
+++ frontend/src/order-calendar/app.js
@@ -3,10 +3,35 @@ import { mobileGuard } from '../common/mobile.guard';
 import { unregisteredUserGuard } from '../common/unregistered-user.guard';
 import { token } from '../common/token.service';
 import { sendForm, preventDefault } from '../common/utils';
+import { generateErrorToast, generateSuccessToast } from '../common/toast';
 
 const API_URL = `${ENVIRONMENT.API_URL}/order`;
 mobileGuard();
 unregisteredUserGuard();
+
+const $consent = document.getElementById('paymentMethod');
+const $direction = document.getElementById('direction');
+const $contactNumber = document.getElementById('contactNumber');
+const $cp = document.getElementById('cp');
+const $city = document.getElementById('city');
+
+const button = document.getElementById('send');
+
+function checkButtonEnabled() {
+  button.disabled = !(
+    $direction.value.length !== 0 &&
+    $contactNumber.value.length !== 0 &&
+    $cp.value.length !== 0 &&
+    $city.value.length !== 0 &&
+    $consent.checked
+  );
+}
+
+$cp.addEventListener('input', checkButtonEnabled);
+$city.addEventListener('input', checkButtonEnabled);
+$contactNumber.addEventListener('input', checkButtonEnabled);
+$direction.addEventListener('input', checkButtonEnabled);
+$consent.addEventListener('input', checkButtonEnabled);
 
 const userToken = token.get();
 const parameters = new URLSearchParams(window.location.search);
@@ -24,9 +49,9 @@ const specifications = document.getElementById('specifications');
 const instructions = document.getElementById('instructions');
 const paymentMethod = document.getElementById('paymentMethod');
 
-async function send(event) {
+async function workPlsss(event) {
   preventDefault(event);
-  await sendForm({
+  const result = await sendForm({
     url: API_URL,
     method: 'POST',
     body: {
@@ -41,8 +66,8 @@ async function send(event) {
     },
     headers: { 'Content-Type': 'application/json', authorization: userToken },
   });
-  if (typeof token.errorCode !== 'undefined') {
-    errorHandling(token);
+  if (typeof result.errorCode !== 'undefined') {
+    errorHandling(result);
   } else {
     window.open(`${ROUTES.hub}/?order=true`, '_self');
   }
@@ -55,6 +80,8 @@ function errorHandling(error) {
     window.location = ROUTES.hub + '?calendarnotfound=true';
   if (error.errorCode === 'CALENDARALREADYORDERED')
     window.location = ROUTES.hub + '?alreadyordered=true';
+  if (error.errorCode === 'NOTENGOUGHTPARTS')
+    window.location = ROUTES.hub + '?error=true';
   generateErrorToast('Un error desconocido ha sucedido');
 }
-window.send = send;
+window.workPlsss = workPlsss;
