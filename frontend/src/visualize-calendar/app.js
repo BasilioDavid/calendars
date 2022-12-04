56bd6946107d36564df37507228eb0eadc6ce2b9
Basilio <basilio.david.g@gmail.com>
2022-12-04 22:06:08 +0100

    fix(frontend/visualize-calendar): improve all

--- frontend/src/visualize-calendar/app.js
+++ frontend/src/visualize-calendar/app.js
@@ -14,39 +14,59 @@ if (calendarId === null) {
   window.location.replace(ROUTES.hub);
 }
 
-document.getElementById('orderCalendar').href =
-  ROUTES.orderCalendar + '?calendarId=' + calendarId;
+const $orderCalendar = document.getElementById('orderCalendar');
 
 const $article = document.getElementById('images');
 
 async function init() {
-  try {
-    const uri = new URLSearchParams({ calendarId });
-    const images = await sendForm({
-      url: API_URL + '?' + uri.toString(),
-      method: 'GET',
-      headers: {
-        authorization: userToken,
-      },
-    });
-    for (const image of images) {
-      const div = document.createElement('div');
-      const header = document.createElement('header');
-      const img = document.createElement('img');
-      const section = document.createElement('section');
-      const p = document.createElement('p');
-      p.textContent = 'Enero';
-      img.src = 'data:image/png;base64, ' + image;
-      header.appendChild(img);
-      section.appendChild(p);
-      div.appendChild(header);
-      div.append(section);
-      $article.appendChild(div);
-    }
-  } catch (e) {
-    console.error(e);
-    throw e;
+  const uri = new URLSearchParams({ calendarId });
+  const images = await sendForm({
+    url: API_URL + '?' + uri.toString(),
+    method: 'GET',
+    headers: {
+      authorization: userToken,
+    },
+  });
+  if (typeof images.errorCode !== 'undefined') {
+    errorHandling(images);
+  } else {
+    loadImages(images);
   }
 }
 
+function loadImages(images) {
+  for (const image of images.images) {
+    const div = document.createElement('div');
+    const header = document.createElement('header');
+    const img = document.createElement('img');
+    const section = document.createElement('section');
+    const p = document.createElement('p');
+    p.textContent = 'Enero';
+    img.src = 'data:image/png;base64, ' + image;
+    header.appendChild(img);
+    section.appendChild(p);
+    div.appendChild(header);
+    div.append(section);
+    $article.appendChild(div);
+  }
+  if (images.imagesLeft == 0) {
+    $orderCalendar.href = ROUTES.orderCalendar + '?calendarId=' + calendarId;
+  } else {
+    $orderCalendar.style.display = 'none';
+    showWarning();
+  }
+}
+
+const $warning = document.getElementById('warning');
+function showWarning() {
+  $warning.style.zIndex = 1;
+}
+
+function errorHandling(error) {
+  if (error.errorCode === 'FORBIDDEN')
+    window.location = ROUTES.login + '?forbidden=true';
+  if (error.errorCode === 'CALENDARNOTFOUND')
+    window.location = ROUTES.hub + '?calendarnotfound=true';
+  generateErrorToast('Un error desconocido ha sucedido');
+}
 init();
