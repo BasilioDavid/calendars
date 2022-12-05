1e424c3d139d721f982b0e222982f6b60210fdbc
Basilio <basilio.david.g@gmail.com>
2022-12-05 08:55:47 +0100

    feat(frontend/register): add form validation

--- frontend/src/register/app.js
+++ frontend/src/register/app.js
@@ -11,7 +11,36 @@ const API_URL = `${ENVIRONMENT.API_URL}/user/register`;
 
 const emailField = document.getElementById('email');
 const passwordField = document.getElementById('password');
+const password2Field = document.getElementById('password2');
 const nameField = document.getElementById('name');
+const button = document.getElementById('send');
+
+function checkButtonEnabled() {
+  button.disabled = !(
+    checkBothPasswordsMatch() &&
+    emailField.value.length !== 0 &&
+    passwordField.value.length !== 0 &&
+    nameField.value.length !== 0 &&
+    password2Field.value.length !== 0
+  );
+}
+
+const errorMessage = document.getElementById('errorMessage');
+function checkBothPasswordsMatch() {
+  console.log('entrno');
+  const match = password2Field.value === passwordField.value;
+  if (!match) {
+    errorMessage.style.display = 'block';
+  } else {
+    errorMessage.style.display = 'none';
+  }
+  return match;
+}
+
+emailField.addEventListener('input', checkButtonEnabled);
+passwordField.addEventListener('input', checkButtonEnabled);
+nameField.addEventListener('input', checkButtonEnabled);
+password2Field.addEventListener('input', checkButtonEnabled);
 
 async function register(event) {
   preventDefault(event);
@@ -35,7 +64,7 @@ async function register(event) {
 
 function errorHandling(error) {
   if (error.errorCode === 'EMAILALREADYFOUND')
-    generateErrorToast('El email ya ha sido registrado');
+    return generateErrorToast('El email ya ha sido registrado');
   generateErrorToast('Un error desconocido ha sucedido');
 }
 
